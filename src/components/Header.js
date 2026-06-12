0f2e752a6f9ce2e9105971c8f683b45fcc931e6e
David Nwachukwu <davidike444@gmail.com>
2026-06-12 18:18:47 -0500

    header designed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,6 @@ function Header() {
   return (
     <header>
       <h1>Task-track</h1>
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-      </nav>
     </header>
   );
 }
